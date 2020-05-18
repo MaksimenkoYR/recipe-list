54560f3fd1bda0c23e9a5fb2a0fcfd517d90e093
CodeJesus24 <maksimenko.yr@gmail.com>
2020-05-18 13:31:17 +0200

    update styles for header

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,11 +3,13 @@ import React from "react";
     const SearchForm = ({doSearch, setSearchQuery}) => {
         return(
         <form  onSubmit={doSearch} className="search-form">
+            <label className="search-form__search-label">
             <input 
-                className="search-form__input" 
+                className="search-form__search-input" 
                 type="text" 
                 onChange={e => {setSearchQuery(e.currentTarget.value)}} 
             />
+            </label>
             <button className="search-form__button button" type="submit">
                 Search
             </button>
